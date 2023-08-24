24d881c09abfadb9908f1b02f53bc3a9738de37a
Wedlec129 <boryasokolov@gmail.com>
2023-08-24 13:07:27 +0300

    Initial Commit

---
+++ TestingMapApp/TestingMapAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestingMapAppApp.swift
+//  TestingMapApp
+//
+//  Created by Борух Соколов on 24.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct TestingMapAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
